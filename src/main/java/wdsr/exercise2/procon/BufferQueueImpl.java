893054fb33f773bf67fd9b5b14eb293d1aa0a11f
barni211 <barni211@gmail.com>
2017-03-27 19:48:21 +0200

    Rozwiazanie z blockingQueue

--- src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
@@ -1,15 +1,26 @@
 package wdsr.exercise2.procon;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
 /**
  * Task: implement Buffer interface using one of *Queue classes from java.util.concurrent package.
  */
 public class BufferQueueImpl implements Buffer {
+	private static BlockingQueue<Order> orderList = new LinkedBlockingQueue<Order>();
+	
 	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+			orderList.put(order);
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+			Order e = (Order)orderList.take();
+			return e;//take();
+			
 	}
+
+	
+
+	
 }
