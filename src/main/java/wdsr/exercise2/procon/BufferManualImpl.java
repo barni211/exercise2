a386ffffb058b9c7715cc8b843045a4f17b2044a
barni211 <barni211@gmail.com>
2017-03-27 21:02:47 +0200

    Praca domowa, wersja z synchronized

--- src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
@@ -1,16 +1,42 @@
 package wdsr.exercise2.procon;
 
+import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Task: implement Buffer interface without using any *Queue classes from java.util.concurrent package.
  * Any combination of "synchronized", *Lock, *Semaphore, *Condition, *Barrier, *Latch is allowed.
  */
 public class BufferManualImpl implements Buffer {
+	private static int size = 100000;
+	private ArrayList<Order> queue = new ArrayList<Order>(size);
+	
+	
 	public void submitOrder(Order order) throws InterruptedException {
+		synchronized(queue)
+		{
+			while(queue.size() == size)
+			{
+				queue.wait();
+			}
+			queue.add(order);
+			queue.notifyAll();
+		}
+		
 		
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+		synchronized(queue)
+		{
+			while(queue.isEmpty())
+			{
+				queue.wait();
+			}
+			Order orderToReturn = queue.remove(0);
+			queue.notifyAll();
+			return orderToReturn;
+		}
 	}
 }
--- src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
@@ -8,15 +8,14 @@ import java.util.concurrent.LinkedBlockingQueue;
  * Task: implement Buffer interface using one of *Queue classes from java.util.concurrent package.
  */
 public class BufferQueueImpl implements Buffer {
-	private static BlockingQueue<Order> orderList = new LinkedBlockingQueue<Order>();
+	private BlockingQueue<Order> orderList = new LinkedBlockingQueue<Order>();
 	
 	public void submitOrder(Order order) throws InterruptedException {
 			orderList.put(order);
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
-			Order e = (Order)orderList.take();
-			return e;//take();
+			return orderList.take();
 			
 	}
 
